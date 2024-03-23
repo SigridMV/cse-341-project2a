db456a8ebc9e666e3c56f46a92230cafde0aee4a
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2024-03-23 01:18:08 -0300

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
+const passport = require('passport');
+
 //Routes Setup
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => { 
+//router.get('/', (req, res) => { 
     //#swagger.tag=['Hello World']
-    res.send('Hello, Welcome to CSE 341 / Sigrid Müller');
-});
+   // res.send('Hello, Welcome to CSE 341 / Sigrid Müller');
+//});
 
 router.use('/movies', require('./movies'));
+
+router.get('/login', passport.authenticate('github'),(req,res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err){
+        if(err){ return next(err); }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
